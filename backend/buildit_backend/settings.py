4d120416a471c38e6796a4e3819656d92b25e403
viignesh14 <viignesh.14@gmail.com>
2026-03-24 20:53:43 +0530

    update backend ALLOWED_HOSTS for render deployment

--- backend/buildit_backend/settings.py
+++ backend/buildit_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-244iv7*zyodp(rf$%ozrofy*#qu-1s-ncnrkbckxs=rr_moui(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
